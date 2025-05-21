3a5f2cd8a8c598dd1ff7048d76392ee3837ebabd
Andrew Phoenix <mrtopnacho@gmail.com>
2025-05-21 16:46:50 -0400

    handle_connection implementation

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::{io::{BufReader, prelude::*}, net::{TcpListener, TcpStream}};
+use std::{fs, io::{BufReader, prelude::*}, net::{TcpListener, TcpStream}};
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
@@ -14,6 +14,10 @@ fn handle_connection(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&stream);
     let http_request: Vec<_> = buf_reader.lines().map(|result| result.unwrap()).take_while(|line| !line.is_empty()).collect();
     
-    let response = "HTTP/1.1 200 OK\r\n\r\n";
+    let status_line = "HTTP/1.1 200 OK\r\n\r\n";
+    let contents = fs::read_to_string("hello.html").unwrap();
+    let length = contents.len();
+
+    let response = format!("{}\r\nContent length: {}\r\n\r\n{}", status_line, length, contents);
     stream.write_all(response.as_bytes()).unwrap();
 }
